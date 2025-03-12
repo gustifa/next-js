173504e6c847468d13a204255c1c88097b9968fc
gustifa <fauzangustifa@gmail.com>
2025-03-12 11:37:16 +0700

    Next-js

--- src/app/api/page.jsx
+++ src/app/api/page.jsx
@@ -1,16 +1,24 @@
-import React, { useEffect } from 'react';
+'use client'
+import React, { useEffect, useState } from 'react';
+async function getData(params) {
+    let res = await fetch('https://dummyjson.com/products')
+    let json = await res.json()
+    return json['products']
+}
+const page = async() => {
+let data = await getData();
+ 
 
-const page = () => {
-
-    useEffect(()=> {
-        (async() => {
-            let res = await fetch('')
-            let json = await res.json()
-        })
-    })
     return (
         <div>
             <h1>Call Api</h1>
+            {data.map((item,index) => {
+                return <div key={index}>
+                    <h4>{item['title']}</h4>
+                    <h4>{item['category']}</h4>
+                    <p>Rp. {item['price']}</p>
+                </div>
+            })}
         </div>
     );
 };
